f7b8c95d7d97d401b92bcfc3f2eb49df504c246b
Gernot Ullrich <gernot.ullrich@gmx.de>
2020-10-10 22:55:56 +0200

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :deploy_to, '/var/www/web1.carombus.de/web/'
 
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
-append :linked_files,'config/database.yml'
+append :linked_files,'config/database.yml', 'config/secrets.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
