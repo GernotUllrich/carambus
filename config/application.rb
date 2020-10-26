9dd6011db297ab1ee664cc28a364acea7fdf7e02
Gernot Ullrich <gernot.ullrich@gmx.de>
2020-10-26 15:32:32 +0100

    Revert "add i18n tools"
    This reverts commit b4fdc8f832620d396b764e210cc5b59cd1724047.

--- config/application.rb
+++ config/application.rb
@@ -36,9 +36,6 @@ module Carambus
       end
     end
 
-    I18n.load_path << Dir[File.expand_path("config/locales") + "/*.yml"]
-    I18n.default_locale = :en
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
