ee92b61f0114d3e26b4cad7e55c55c5f4d0edee5
Gernot Ullrich <gernot.ullrich@gmx.de>
2020-11-21 16:21:34 +0100

    datatables via webpacker

---
+++ README.md
@@ -0,0 +1,6 @@
+# README
+
+Rails 6 with Webpack and Yarn demo app with:
+- Bootstrap (bundle) to load the complete JS utilitaries from BS
+- Data Tables: https://datatables.net/
+- jQuery notifications from bootstrap-growl-ifightcrime: Pretty simple jQuery plugin that turns standard Bootstrap alerts into hovering 'Growl-like' notifications. (https://yarnpkg.com/en/package/bootstrap-growl-ifightcrime)
--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,6 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-#require "active_model/railtie"
-#require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "sprockets/railtie"
-require "action_cable/engine"
-# require "rails/test_unit/railtie"
-
-require 'autoprefixer-rails'
-require 'slim-rails'
-require 'uglifier'
-
-require 'bootstrap'
-require 'jquery-rails'
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -25,31 +8,12 @@ Bundler.require(*Rails.groups)
 
 module Carambus
   class Application < Rails::Application
-    #config.api_only = true
-    config.assets.enabled = true if config.assets.respond_to?(:enabled)
-    if Rails::VERSION::MAJOR > 4
-      # Rails 4 precompiles application.css|js by default, but future version of Rails do not.
-      config.assets.precompile += %w( application.css application.js )
-    end
-    config.to_prepare do
-      if ENV['VERBOSE']
-        STDERR.puts "Loaded Rails #{Rails::VERSION::STRING}, Sprockets #{Sprockets::VERSION}",
-                    "Asset paths: #{Rails.application.config.assets.paths}"
-      end
-    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
