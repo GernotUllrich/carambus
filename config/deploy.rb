852bfa8a5055a135b181203b074b5b32a9b9c52c
Gernot Ullrich <gernot.ullrich@gmx.de>
2021-06-06 19:55:58 +0200

    no message

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,8 @@ set :deploy_to, '/var/www/carambus'
 
 # Default value for :linked_files is []
 # append :linked_files, 'config/database.yml', 'config/secrets.yml'
-append :linked_files,'config/database.yml', 'config/credentials/production.key'
+#append :linked_files,'config/database.yml', 'config/credentials/production.key'
+append :linked_files,'config/database.yml', 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'node_modules', 'public/packs', 'public/assets'
