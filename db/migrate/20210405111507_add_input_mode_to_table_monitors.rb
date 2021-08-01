64332d6c5056519635f14d63b4fa9765d32338ce
Gernot Ullrich <gernot.ullrich@gmx.de>
2021-08-01 12:15:29 +0200

    fix migration

--- db/migrate/20210405111507_add_input_mode_to_table_monitors.rb
+++ db/migrate/20210405111507_add_input_mode_to_table_monitors.rb
@@ -1,4 +1,4 @@
-class AddInputModeToTableMonitors < ActiveRecord::Migration[6.1]
+class AddInputModeToTableMonitors < ActiveRecord::Migration[6.0]
   def change
     add_column :table_monitors, :panel_state, :string, default: "pointer_mode", null: false
     add_column :table_monitors, :current_element, :string, default: "pointer_mode", null: false
--- db/migrate/20210419205544_add_timer_job_id_to_table_monitors.rb
+++ db/migrate/20210419205544_add_timer_job_id_to_table_monitors.rb
@@ -1,4 +1,4 @@
-class AddTimerJobIdToTableMonitors < ActiveRecord::Migration[6.1]
+class AddTimerJobIdToTableMonitors < ActiveRecord::Migration[6.0]
   def change
     add_column :table_monitors, :timer_job_id, :string
   end
