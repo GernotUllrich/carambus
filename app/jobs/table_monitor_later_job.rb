af0a997c455215219fabbbb375e308af2bf3e0a8
Gernot Ullrich <gernot.ullrich@gmx.de>
2021-09-02 09:43:13 +0200

    update table_monitor view as well

--- app/jobs/table_monitor_later_job.rb
+++ app/jobs/table_monitor_later_job.rb
@@ -10,14 +10,14 @@ class TableMonitorLaterJob < ApplicationJob
       locals: { table_monitor: table_monitor, full_screen: false }
     )
 
-    # html_current_games = ApplicationController.render(
-    #   partial: "tournament_monitors/current_games",
-    #   locals: { tournament_monitor: table_monitor.tournament_monitor }
-    # )
-    # cable_ready["table-monitor-stream-later"].inner_html(
-    #   selector: "#tournament_monitor_current_games_#{table_monitor.tournament_monitor.andand.id}",
-    #   html: html_current_games
-    # )
+    html_current_games = ApplicationController.render(
+      partial: "tournament_monitors/current_games",
+      locals: { tournament_monitor: table_monitor.tournament_monitor }
+    )
+    cable_ready["table-monitor-stream-later"].inner_html(
+      selector: "#tournament_monitor_current_games_#{table_monitor.tournament_monitor.andand.id}",
+      html: html_current_games
+    )
     cable_ready["table-monitor-stream-later"].inner_html(
       selector: "#table_monitor_#{table_monitor.id}",
       html: html
