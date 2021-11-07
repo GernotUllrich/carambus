3bfdbd413283cd7c1c713e877a3845b7ed4809d6
Gernot Ullrich <gernot.ullrich@gmx.de>
2021-11-07 13:50:49 +0100

    fix migration

--- db/migrate/20210908094135_add_table_monitor_id_to_tables.rb
+++ db/migrate/20210908094135_add_table_monitor_id_to_tables.rb
@@ -1,6 +1,6 @@
 class AddTableMonitorIdToTables < ActiveRecord::Migration[6.0]
   def change
-    TableMonitor.destroy_all
+    #TableMonitor.destroy_all
     add_column :tables, :table_monitor_id, :integer
     remove_column :table_monitors, :table_id, :integer
   end
