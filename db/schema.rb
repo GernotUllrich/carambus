37e75aaae5fdc59dff13b0e0f00addf414f2e282
Gernot Ullrich <gernot.ullrich@gmx.de>
2022-04-24 07:55:02 +0200

    add base url to region_ccs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_23_210340) do
+ActiveRecord::Schema.define(version: 2022_04_24_055056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -457,6 +457,7 @@ ActiveRecord::Schema.define(version: 2022_04_23_210340) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "base_url"
   end
 
   create_table "regions", force: :cascade do |t|
