7e9fb5a671a44435eb15c2380f037b1e125dd992
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-04-11 17:30:17 +0200

    avoid exceptions while not logged in

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,36 +6,36 @@ class PagesController < ApplicationController
   before_action :set_page, only: [:show, :edit, :update, :destroy, :publish, :archive]
   before_action :authorize_page, except: [:index, :new, :create]
   before_action :authorize_action, only: [:new, :create]
-  
+
   # GET /pages
   def index
     @pages = Page.accessible_to(current_user)
                 .includes(:super_page)
                 .order('super_page_id NULLS FIRST, position')
-    
+
     # Filter by tag if provided
     if params[:tag].present?
       @pages = @pages.where("tags @> ?", [params[:tag]].to_json)
       @tag = params[:tag]
     end
-    
+
     # Only show root pages in the main index
     @root_pages = @pages.root_pages.ordered
   end
-  
+
   # GET /pages/1
   def show
     @sub_pages = @page.sub_pages
                      .accessible_to(current_user)
                      .ordered
   end
-  
+
   # GET /pages/new
   def new
     @page = Page.new(super_page_id: params[:super_page_id])
     @parent_pages = Page.accessible_to(current_user, 'update').ordered
   end
-  
+
   # GET /pages/1/edit
   def edit
     @parent_pages = Page.accessible_to(current_user, 'update')
@@ -43,12 +43,12 @@ class PagesController < ApplicationController
                        .where.not(id: @page.sub_pages.pluck(:id))
                        .ordered
   end
-  
+
   # POST /pages
   def create
     @page = Page.new(page_params)
     @page.author = current_user
-    
+
     if @page.save
       redirect_to @page, notice: 'Page was successfully created.'
     else
@@ -56,7 +56,7 @@ class PagesController < ApplicationController
       render :new
     end
   end
-  
+
   # PATCH/PUT /pages/1
   def update
     if @page.update(page_params)
@@ -69,42 +69,42 @@ class PagesController < ApplicationController
       render :edit
     end
   end
-  
+
   # DELETE /pages/1
   def destroy
     @page.destroy
     redirect_to pages_url, notice: 'Page was successfully deleted.'
   end
-  
+
   # POST /pages/1/publish
   def publish
     @page.publish
     redirect_to @page, notice: 'Page was successfully published.'
   end
-  
+
   # POST /pages/1/archive
   def archive
     @page.archive
     redirect_to @page, notice: 'Page was successfully archived.'
   end
-  
+
   # POST /pages/preview
   def preview
     content = params[:content]
-    
+
     # Create a temporary page object to render the content
     page = Page.new(content: content, content_type: 'markdown')
-    
+
     render json: { html: page.rendered_content }
   end
-  
+
   private
-  
+
   # Use callbacks to share common setup or constraints between actions
   def set_page
     @page = Page.find(params[:id])
   end
-  
+
   # Only allow a list of trusted parameters through
   def page_params
     params.require(:page).permit(
@@ -113,7 +113,7 @@ class PagesController < ApplicationController
       crud_minimum_roles: [:create, :read, :update, :delete]
     )
   end
-  
+
   # Authorize the page for the current action
   def authorize_page
     action = case params[:action]
@@ -122,18 +122,18 @@ class PagesController < ApplicationController
              when 'destroy' then 'delete'
              else params[:action]
              end
-    
+
     unless @page.accessible_to?(current_user, action)
       flash[:alert] = "You don't have permission to #{action} this page."
       redirect_to pages_path
     end
   end
-  
+
   # Authorize the action for creating new pages
   def authorize_action
-    unless Page.accessible_to(current_user, 'create').exists? || current_user.system_admin?
+    unless Page.accessible_to(current_user, 'create').exists? || current_user&.system_admin?
       flash[:alert] = "You don't have permission to create pages."
       redirect_to pages_path
     end
   end
-end 
+end
