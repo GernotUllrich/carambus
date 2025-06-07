176e22a336df12ba6677602dee4afb4d213940c5
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-07 14:18:20 +0200

    ....

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -36,7 +36,7 @@ Rails.application.config.content_security_policy do |policy|
     policy.script_src  :self, :https, :unsafe_inline, :unsafe_eval,
       "https://cdn.jsdelivr.net",
       "https://unpkg.com"
-    policy.style_src   :self, "https://rsms.me", :nonce
+    policy.style_src   :self, "https://rsms.me", :unsafe_inline
     policy.connect_src :self, :https, "ws://localhost:3000", "wss://localhost:3000"
   else
     # Production policy with strict nonces
@@ -45,8 +45,7 @@ Rails.application.config.content_security_policy do |policy|
     policy.img_src     :self, :https, :data
     policy.object_src  :none
     policy.script_src  :self, :https, "https://cdn.jsdelivr.net", "https://newapi.carambus.de"
-    # Remove unsafe-inline since we're using nonces
-    policy.style_src   :self, :https, "https://rsms.me", :nonce
+    policy.style_src   :self, :https, "https://rsms.me"
     policy.connect_src :self, :https, "wss://#{Carambus.config.carambus_domain}"
   end
 
