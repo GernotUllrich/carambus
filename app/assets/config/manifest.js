f5edd3ddb116825d4fd9da11dce7c2c95e79ce85
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-07 17:05:24 +0200

    ...

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
 //= link rails-ujs.js
 //= link scoreboard.css
 //= link scoreboard_welcome.css
