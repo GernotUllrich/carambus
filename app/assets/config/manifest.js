34697eec121097724d1552a8a5a0740e7b36c0a9
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-07 16:50:33 +0200

    CSP fixes

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,14 @@
 //= link_tree ../builds
 //= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link rails-ujs.js
 //= link scoreboard.css
 //= link scoreboard_welcome.css
+//= link scoreboard_free_game_karambol.css
+//= link scoreboard_free_game_pool.css
+//= link scoreboard_tournament_scores.css
+//= link application.css
+//= link application.tailwind.css
 
