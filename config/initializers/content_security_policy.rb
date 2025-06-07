eea95f54f8ff2ad86749e9a34c4865aa3ee271ab
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-07 12:33:10 +0200

    fix content security

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -29,23 +29,35 @@
 Rails.application.config.content_security_policy do |policy|
   if Rails.env.development?
     # Development policy with unsafe-inline
-    policy.style_src :self, :unsafe_inline, "https://rsms.me"
-    policy.script_src :self, :unsafe_inline, :unsafe_eval,
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https, :unsafe_inline, :unsafe_eval,
       "https://cdn.jsdelivr.net",
       "https://unpkg.com"
+    policy.style_src   :self, :unsafe_inline, "https://rsms.me"
+    policy.connect_src :self, :https, "ws://localhost:3000", "wss://localhost:3000"
   else
     # Production policy with strict nonces
-    policy.style_src :self, 'https://rsms.me', :strict_dynamic
-    policy.script_src :self, 'https://cdn.jsdelivr.net', :strict_dynamic
-  end
+    policy.default_src :self, :https
+    policy.font_src    :self, :https, :data
+    policy.img_src     :self, :https, :data
+    policy.object_src  :none
+    policy.script_src  :self, :https, "https://cdn.jsdelivr.net", :strict_dynamic
+    policy.style_src   :self, :https, "https://rsms.me", :strict_dynamic
+    policy.connect_src :self, :https, "wss://#{Carambus.config.carambus_domain}"
 
-  # Consider adding these for production as well (without unsafe_inline/unsafe_eval)
-  # policy.style_src :self, "https://rsms.me"
-  # policy.script_src :self, "https://unpkg.com"
+    # Add asset-src directive to allow loading of assets
+    policy.asset_src   :self, :https
 end
 
-# Nonce generation only in production
-if Rails.env.production?
+  # Generate session nonces for permitted importmap, inline scripts, and inline styles
   Rails.application.config.content_security_policy_nonce_generator = ->(request) { SecureRandom.base64(16) }
   Rails.application.config.content_security_policy_nonce_directives = %w(script-src style-src)
+end
+
+# Report CSP violations in production
+if Rails.env.production?
+  Rails.application.config.content_security_policy_report_only = false
 end
