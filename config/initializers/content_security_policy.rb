9d41fc141dd8713fbec16a8ca1438117fe8b488d
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-07 12:42:29 +0200

    no message

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -44,8 +44,8 @@ Rails.application.config.content_security_policy do |policy|
     policy.font_src    :self, :https, :data
     policy.img_src     :self, :https, :data
     policy.object_src  :none
-    policy.script_src  :self, :https, "https://cdn.jsdelivr.net", :strict_dynamic
-    policy.style_src   :self, :https, "https://rsms.me", :strict_dynamic
+    policy.script_src  :self, :https, "https://cdn.jsdelivr.net", "https://newapi.carambus.de"
+    policy.style_src   :self, :https, "https://rsms.me", :unsafe_inline
     policy.connect_src :self, :https, "wss://#{Carambus.config.carambus_domain}"
   end
 
