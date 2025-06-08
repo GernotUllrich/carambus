2adabb1d828bf8475d23459b90ebcbe5cd0a56d0
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-08 13:22:36 +0200

    make puma startup parameterizable

--- bin/manage-puma.sh
+++ bin/manage-puma.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-if ps aux | grep puma.carambus2_production | grep -v grep
+BASENAME=$1
+if pgrep -f "puma.${BASENAME}_production" > /dev/null
+
 then
   echo "Service is running, performing phased-restart"
-  cd /var/www/carambus/current; RAILS_ENV=production /var/www/.rbenv/shims/bundle exec pumactl -F /var/www/carambus/shared/config/puma.rb phased-restart
+  cd /var/www/${BASENAME}/current; RAILS_ENV=production /var/www/.rbenv/shims/bundle exec pumactl -F /var/www/${BASENAME}/shared/config/puma.rb phased-restart
 else
   echo "Service is not running, starting service"
-  cd /var/www/carambus/current; sudo systemctl start puma.service
+  cd /var/www/${BASENAME}/current; sudo systemctl start puma-${BASENAME}.service
 fi
 
