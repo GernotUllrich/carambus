6a15f7c3312fbb33df4ff67976b6da512b8db30b
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-06-08 14:29:30 +0200

    fix puma restart

--- bin/manage-puma.sh
+++ bin/manage-puma.sh
@@ -4,7 +4,7 @@ if pgrep -f "puma.${BASENAME}_production" > /dev/null
 
 then
   echo "Service is running, performing phased-restart"
-  cd /var/www/${BASENAME}/current; RAILS_ENV=production /var/www/.rbenv/shims/bundle exec pumactl -F /var/www/${BASENAME}/shared/config/puma.rb phased-restart
+  cd /var/www/${BASENAME}/current; RAILS_ENV=production sudo /var/www/.rbenv/shims/bundle exec pumactl -F /var/www/${BASENAME}/shared/config/puma.rb phased-restart
 else
   echo "Service is not running, starting service"
   cd /var/www/${BASENAME}/current; sudo systemctl start puma-${BASENAME}.service
