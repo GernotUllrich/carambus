b4c4f89d1ed9b5ccde1e08d7c16e9857b1d96110
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-07-20 22:33:18 +0200

    refactor: Replace static documentation with MkDocs links
    - Add league.md documentation in German and English
    - Create MkDocsUrls configuration module
    - Update navigation to use MkDocs documentation links
    - Remove static doc_tournament and doc_league controller methods
    - Remove static documentation view files
    - Update mkdocs.yml navigation to include league documentation

---
+++ config/initializers/mkdocs_urls.rb
@@ -0,0 +1,125 @@
+# frozen_string_literal: true
+
+# MkDocs Documentation URLs Configuration
+# This file defines the URLs for the MkDocs-generated documentation
+# that replaces the static documentation pages
+
+module MkDocsUrls
+  # Base URL for the MkDocs documentation
+  # This should be updated when the documentation is deployed
+  BASE_URL = ENV.fetch('MKDOCS_BASE_URL', 'https://gernotullrich.github.io/carambus-docs').freeze
+
+  # Documentation URLs for different languages and sections
+  class << self
+    def tournament_doc_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/tournament/"
+      when 'en'
+        "#{BASE_URL}/en/tournament/"
+      else
+        "#{BASE_URL}/de/tournament/" # Default to German
+      end
+    end
+
+    def league_doc_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/league/"
+      when 'en'
+        "#{BASE_URL}/en/league/"
+      else
+        "#{BASE_URL}/de/league/" # Default to German
+      end
+    end
+
+    def developer_guide_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/DEVELOPER_GUIDE/"
+      when 'en'
+        "#{BASE_URL}/en/DEVELOPER_GUIDE/"
+      else
+        "#{BASE_URL}/de/DEVELOPER_GUIDE/" # Default to German
+      end
+    end
+
+    def api_doc_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/API/"
+      when 'en'
+        "#{BASE_URL}/en/API/"
+      else
+        "#{BASE_URL}/de/API/" # Default to German
+      end
+    end
+
+    def database_design_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/database_design/"
+      when 'en'
+        "#{BASE_URL}/en/database_design/"
+      else
+        "#{BASE_URL}/de/database_design/" # Default to German
+      end
+    end
+
+    def scoreboard_setup_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/scoreboard_autostart_setup/"
+      when 'en'
+        "#{BASE_URL}/en/scoreboard_autostart_setup/"
+      else
+        "#{BASE_URL}/de/scoreboard_autostart_setup/" # Default to German
+      end
+    end
+
+    def table_reservation_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/table_reservation_heating_control/"
+      when 'en'
+        "#{BASE_URL}/en/table_reservation_heating_control/"
+      else
+        "#{BASE_URL}/de/table_reservation_heating_control/" # Default to German
+      end
+    end
+
+    def mode_switcher_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/mode_switcher/"
+      when 'en'
+        "#{BASE_URL}/en/mode_switcher/"
+      else
+        "#{BASE_URL}/de/mode_switcher/" # Default to German
+      end
+    end
+
+    def mkdocs_documentation_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/mkdocs_dokumentation/"
+      when 'en'
+        "#{BASE_URL}/en/mkdocs_documentation/"
+      else
+        "#{BASE_URL}/de/mkdocs_dokumentation/" # Default to German
+      end
+    end
+
+    # Main documentation index
+    def main_doc_url(locale = I18n.locale)
+      case locale.to_s
+      when 'de'
+        "#{BASE_URL}/de/"
+      when 'en'
+        "#{BASE_URL}/en/"
+      else
+        "#{BASE_URL}/de/" # Default to German
+      end
+    end
+  end
+end 
