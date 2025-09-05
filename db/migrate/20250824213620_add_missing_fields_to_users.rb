1edee0aa66e807a2537224ce75f4f6c9f4f0c069
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-09-05 15:17:48 +0200

    fix migration with missing fields in users model

--- db/migrate/20250824213620_add_missing_fields_to_users.rb
+++ db/migrate/20250824213620_add_missing_fields_to_users.rb
@@ -1,2 +1,10 @@
 class AddMissingFieldsToUsers < ActiveRecord::Migration[7.2]
+  add_column :users, :otp_required_for_login, :boolean
+  add_column :users, :otp_secret, :string
+  add_column :users, :last_otp_timestep, :integer
+  add_column :users, :otp_backup_codes, :text
+  add_column :users, :code, :string
+  add_column :users, :preferences, :jsonb
+  add_column :users, :name, :string, as: "(((first_name)::text || ' '::text) || (COALESCE(last_name, ''::character varying))::text)", stored: true
+  add_column :users, :role, :jsonb, default: 0
 end
