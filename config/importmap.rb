2f2903f7535aefde06ed3cdd4753357935a426bc
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-09-17 19:47:41 +0200

    Revert to Sprockets asset pipeline
    - Change application.html.erb back to javascript_include_tag
    - Remove importmap configuration and restore Sprockets setup
    - Move scoreboard_utils.js back to app/javascript/ root
    - Fix AssetNotPrecompiledError by using Sprockets instead of importmap
    - User prefers Sprockets asset pipeline in both development and production

--- config/importmap.rb
+++ config/importmap.rb
@@ -3,14 +3,3 @@
 # Pin npm packages by running ./bin/importmap
 
 pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin_all_from "app/javascript/controllers", under: "controllers"
-pin_all_from "app/javascript/channels", under: "channels"
-pin_all_from "app/javascript/src", under: "src"
-pin_all_from "app/javascript/utils", under: "utils"
-pin_all_from "app/javascript/utilities", under: "utilities"
-pin "@rails/activestorage", to: "activestorage.esm.js"
-pin "local-time", to: "local-time"
-pin "@stimulus_reflex/polyfills", to: "stimulus_reflex_polyfills.js"
