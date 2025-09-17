d69c95d30771c77e46bee85dfa3858656c57ee47
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-09-17 20:22:52 +0200

    Fix dropdown controller conflict
    - Remove TailwindCSS dropdown component registration that was overriding custom dropdown_controller.js
    - Custom dropdown controller now properly handles dropdown functionality
    - Fixes dropdown menus not working in development scenarios

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -8,8 +8,9 @@ controllers.forEach((controller) => {
 
 import controller from '../controllers/application_controller'
 
-import { Dropdown } from "tailwindcss-stimulus-components"
-application.register('dropdown', Dropdown)
+// Removed TailwindCSS dropdown component to avoid conflict with custom dropdown_controller.js
+// import { Dropdown } from "tailwindcss-stimulus-components"
+// application.register('dropdown', Dropdown)
 
 import "@stimulus_reflex/polyfills"
 import StimulusReflex from 'stimulus_reflex'
