2c3705fe7a36c82f3ad6c69b0ecdd879764d6b98
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-09-17 10:12:34 +0200

    Fix ActionCable pubsub adapter configuration - add missing adapter config

--- config/cable.rb
+++ config/cable.rb
@@ -4,7 +4,17 @@
 ActionCable.server.config.disable_request_forgery_protection = true
 ActionCable.server.config.allow_same_origin_as_host = true
 
-# Force ActionCable to use HTTP instead of HTTPS
-if Rails.env.production?
-  ActionCable.server.config.url = "ws://192.168.178.107:82/cable"
+# Configure pubsub adapter for ActionCable
+Rails.application.configure do
+  config.action_cable.mount_path = '/cable'
+  config.action_cable.disable_request_forgery_protection = true
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
+  
+  # Use async adapter for both development and production
+  config.action_cable.adapter = :async
+  
+  # Set URL for production
+  if Rails.env.production?
+    config.action_cable.url = "ws://192.168.178.107:82/cable"
+  end
 end
