a9ecc705c51a7165aa539a9ce49590e7ac08a570
Gernot Ullrich <gernot.ullrich@gmx.de>
2025-11-02 16:21:35 +0100

    Add debug logging to LocalProtector to diagnose sync issue

--- app/models/local_protector.rb
+++ app/models/local_protector.rb
@@ -26,7 +26,10 @@ module LocalProtector
     #   Carambus.config.carambus_api_url.present?
     # end
     def disallow_saving_global_records
-      raise ActiveRecord::Rollback if id < 50_000_000 && ApplicationRecord.local_server? && !unprotected
+      if id < 50_000_000 && ApplicationRecord.local_server? && !unprotected
+        Rails.logger.warn("LocalProtector: Blocking save of global #{self.class.name}[#{id}], unprotected=#{unprotected.inspect}")
+        raise ActiveRecord::Rollback
+      end
 
       true
     end
