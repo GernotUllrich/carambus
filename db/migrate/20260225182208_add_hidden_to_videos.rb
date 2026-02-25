6a1be726957533bd9ee0415facc9be4a2e783c5d
Gernot Ullrich <gernot.ullrich@gmx.de>
2026-02-25 20:40:38 +0100

    ..

--- db/migrate/20260225182208_add_hidden_to_videos.rb
+++ db/migrate/20260225182208_add_hidden_to_videos.rb
@@ -1,7 +1,8 @@
 class AddHiddenToVideos < ActiveRecord::Migration[7.2]
   disable_ddl_transaction!
+
   def change
     add_column :videos, :hidden, :boolean, default: false, null: false
-    add_index :videos, :hidden
+    add_index :videos, :hidden, algorithm: :concurrently
   end
 end
